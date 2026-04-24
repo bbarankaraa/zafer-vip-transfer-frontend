69714f46bae6a07e002e8602b8810ec772c354c3
Arda Baran Kara <141037336+bbarankaraa@users.noreply.github.com>
2026-04-24 12:08:43 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  },
 };
 
 export default nextConfig;
